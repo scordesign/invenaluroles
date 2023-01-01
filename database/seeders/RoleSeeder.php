a22070342d1602665ab92fa14951a7e48052e0a3
Seb1995 <99344424+scordesign@users.noreply.github.com>
2023-01-01 12:52:18 -0500

    Create RoleSeeder.php

---
+++ database/seeders/RoleSeeder.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Spatie\Permission\Models\Role;
+use Spatie\Permission\Models\Permission;
+
+class RoleSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //
+        $role1 = Role::create('name' => 'admin');
+        $role2 = Role::create('name' => 'digitador');
+
+        Permission::create (['name'] => 'bodegas')
+        Permission::create (['name'] => 'grupos')
+        Permission::create (['name'] => 'ubicaciones')
+        Permission::create (['name'] => 'bodegas')
+
+    }
+}
