16cba13fb25eac78bc65fcc1c95ddde664163e90
Seb1995 <99344424+scordesign@users.noreply.github.com>
2023-01-01 23:15:51 -0500

    Update app.blade.php

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -45,7 +45,7 @@
                             </a></li>@endrole
                             @role('admin')
                             <li class="nav-item">
-                            <a class="nav-link" href="https://127.0.0.1/excel">
+                            <a class="nav-link" href="localhost/excel">
                                 {{ __('Excel Conteo Fisico') }}
                             </a></li>
                         <li class="nav-item">
