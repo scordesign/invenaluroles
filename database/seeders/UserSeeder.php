1d7862ab0bff7937dd4dd4aab671b7fa9a4277a5
Seb1995 <99344424+scordesign@users.noreply.github.com>
2023-01-01 21:16:10 -0500

    Update UserSeeder.php

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -21,7 +21,23 @@ class UserSeeder extends Seeder
             'email' => 'admin@admin.com',
             'password' => bcrypt('admin')
         ])->assignRole('admin');
+        
+        User::create([
+            'name' => 'Lilian Castro',
+            'email' => 'lilian.castro@aluicaconstrucciones.com',
+            'password' => bcrypt('Lilian2023.')
+        ])->assignRole('admin');
 
-        User::factory(9)->create();
+        User::create([
+            'name' => 'Angelica Palacios',
+            'email' => 'angelica.palacios@aluicaconstrucciones.com',
+            'password' => bcrypt('Angelica2023.')
+        ])->assignRole('admin');
+
+        User::create([
+            'name' => 'Lisbeth',
+            'email' => 'costos@aluicaconstrucciones.com',
+            'password' => bcrypt('Lisbeth2023.')
+        ])->assignRole('admin');
         }
 }
